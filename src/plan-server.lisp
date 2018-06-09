e4cd3c302f1f87e9d8ebed548d16544cb735640f
Peter Denno <podenno@gmail.com>
2018-06-08 22:15:31 -0400

    Trivial cleanup

--- src/plan-server.lisp
+++ src/plan-server.lisp
@@ -1,18 +1,15 @@
 (in-package :s2zmq)
 
-(defparameter *server-put* "tcp://127.0.0.1:31728") ; Server
-(defparameter *server-get* "tcp://127.0.0.1:31727") ; Server
+(defparameter *server-put* "tcp://127.0.0.1:31728")
+(defparameter *server-get* "tcp://127.0.0.1:31727")
 
 (defun ask-shop2 (request-str)
   "Eval request string in :shop2-user package."
   (handler-case 
       (let* ((*package* (find-package :shop2-user))
-	     (form (read-from-string request-str))) ; zmq:recv is blocking
+	     (form (read-from-string request-str)))
 	(eval form))
     (serious-condition () :bad-input)))
-
-(defvar *last-msg* nil)
-(defvar *loop-count* 0)
 
 (defun kill-server ()
   "Kill the SHOP2-Server"
@@ -21,8 +18,8 @@
     (when kt
       (sb-thread:terminate-thread kt))))
 
-;;; bind    - create an endpoint, accept connections on socket (to send).
-;;; connect - connect to an endpoint                           (to receive).
+;;; bind    - create an endpoint, accept connections on socket
+;;; connect - connect to an endpoint
 (defun start-server ()
   "Start the server that listens on *server-get*."
   (sb-thread:make-thread
@@ -33,24 +30,7 @@
 	 (zmq:bind respond-client *server-put*)
 	 (zmq:bind listen-client  *server-get*)
 	 (loop
-	    (let* ((got (zmq:recv listen-client 100000)) ; block, waiting for input. NOPE!
-	           (result (ask-shop2 got))) ; block while planning
+	    (let* ((got (zmq:recv listen-client 100000)) ; block, waiting for input. 
+	           (result (ask-shop2 got))) 
 	      (zmq:send respond-client (format nil "~S" result))))))))
      :name "SHOP2-Server"))
-
-  
-
-
-
-
-
-     
-		      
-	      
-
-       
-  
-
-
-
-
