300cc820eaaec9bb699df9f8d90083022de69768
Peter Denno <podenno@gmail.com>
2018-07-08 09:28:43 -0400

    Added clausifier.

--- load.lisp
+++ load.lisp
@@ -6,11 +6,14 @@
 (ql:quickload "bordeaux-threads")
 (ql:quickload "zeromq")
 (ql:quickload "shop2")
+(ql:quickload "cl-ppcre")
 
 (pushnew :plan-server *features*)
 (load (compile-file "/Users/pdenno/Documents/git/shop2-zmq/src/package.lisp"))
 (load (compile-file "/Users/pdenno/Documents/git/shop2-zmq/src/plan-server.lisp"))
+(load (compile-file "/Users/pdenno/Documents/git/shop2-zmq/src/pod-utils/packages.lisp"))
+(asdf:load-system :kif)
 ;(asdf:load-system :shop2-zmq)
 
-(sb-ext:save-lisp-and-die "zeromq-shop2-2018-06-09" :purify t :executable t :toplevel 's2zmq:start-server)
+;(sb-ext:save-lisp-and-die "zeromq-shop2-kif-2018-07-08" :purify t :executable t :toplevel 's2zmq:start-server)
 
